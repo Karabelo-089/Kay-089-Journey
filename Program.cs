538748f59f85428f4d068b0a6ffb1ce8a83d368a
Karabelo-089 <159622267+Karabelo-089@users.noreply.github.com>
2025-06-03 16:06:12 +0200

    Add files via upload
    i was to create the output using all that i have learned up to combining verbatim literals and string interpolation

--- Program.cs
+++ Program.cs
@@ -1,42 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-//Console.WriteLine("Hello, World!");
-Console.WriteLine("Congratulations!");
-Console.Write("");
-Console.Write("You wrote your first lines of code.");
-Console.WriteLine();
-Console.WriteLine('b');
-Console.WriteLine(123);
-Console.WriteLine(0.25F);
-Console.WriteLine(2.625);
-Console.WriteLine(12.39816m);
-Console.WriteLine(true);
-Console.WriteLine(false);
-char userOption;
-int gameScore;
-decimal particlesPerMillion;
-bool processedCustomer;
-// learning Character escape sequence 
-Console.WriteLine("Hello\nWorld!");
-Console.WriteLine("Hello\tWorld!");
-Console.WriteLine("Hello \"World\"!");
-Console.WriteLine("c:\\source\\repos");
-// format output using character escape sequence
-Console.WriteLine("Generating invoices for customer \"Contoso Corp\"...\n");
-Console.WriteLine("Invoice: 1021\t\tComplete!");
-Console.WriteLine("Invoice: 1022\t\tComplete!");
-Console.WriteLine("\nOutput Directory:\t");
-//Learning Verbatim string litral
-Console.WriteLine(@"    c:\source\repos
-(this is where your code goes)");
-//Learning Unicode escape characters
-//Console.WriteLine(@"    c:\source\repos
-//(this is where your code goes)");
-Console.WriteLine(@"c:\invoices");
-//Learning Unicode escape characters(use the \u escape sequence)
-// Kon'nichiwa World(UTF-16)
-Console.WriteLine("\u3053\u3093\u306B\u3061\u306F World!");
-// To generate Japanese invoices:
-// Nihon no seikyu-sho o seisei suru ni wa:
-Console.WriteLine("\n\n\u65e5\u672c\u306e\u8acb\u6c42\u66f8\u3092\u751f\u6210\u3059\u308b\u306b\u306f\uff1a\n\t");
-// User command to run an application
-Console.WriteLine(@"c:\invoices\app.exe -j");
+string projectName = "ACME";
+string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434:";
+Console.WriteLine("View English output:");
+Console.Write("\t");
+Console.WriteLine($@"c:\Exercise\{projectName}\data.txt");
+Console.Write("\n");
+Console.WriteLine(russianMessage);
+Console.Write("\t");
+Console.WriteLine($@"c:\Exercise\{projectName}\ru-RU\data.txt");
+// Another of completing this challenge using only two instructions
+string projectName1 = "ACME";
+string englishLocation = $@"c:\Exercise\{projectName1}\data.txt";
+Console.WriteLine($"View English output:\n\t\t{englishLocation}\n");
+
+string russianMessage1 = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
+string russianLocation = $@"c:\Exercise\{projectName1}\ru-RU\data.txt";
+Console.WriteLine($"{russianMessage1}:\n\t\t{russianLocation}\n");
